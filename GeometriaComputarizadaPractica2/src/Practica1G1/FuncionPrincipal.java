51d7a4120a372e21fb9ad0306d6fc91fb25909a7
Axel <77419228+Axel828388@users.noreply.github.com>
2025-03-14 00:35:31 -0600

    Delete GeometriaComputarizadaPractica2 directory

--- GeometriaComputarizadaPractica2/src/Practica1G1/FuncionPrincipal.java
+++
@@ -1,8 +0,0 @@
-package Practica1G1;
-public class FuncionPrincipal {
-    public static void main(String[] args) {
-        VentanaPrincipal ventana = new VentanaPrincipal();
-        ventana.setVisible(true);
-        
-    }
-}
--- GeometriaComputarizadaPractica2/src/Practica2G1/FuncionPrincipal.java
+++
@@ -1,7 +0,0 @@
-package Practica2G1;
-public class FuncionPrincipal {
-    public static void main(String[] args) {
-        VentanaPrincipal ventana = new VentanaPrincipal();
-        ventana.setVisible(true);
-    }
-}
